4988b17c5205ce3273599c113c72aa7cfb461e6b
richwill28 <e0550368@u.nus.edu>
2021-06-04 15:23:32 +0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+// import App from './App';
 import LogIn from './pages/login';
 import reportWebVitals from './reportWebVitals';
 
