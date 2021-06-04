0d4c66e4188602570a1437129725723dcf8dcffe
richwill28 <e0550368@u.nus.edu>
2021-06-04 15:09:18 +0700

    Create login page

--- src/index.js
+++ src/index.js
@@ -2,11 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import LogIn from './pages/login';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <LogIn />
   </React.StrictMode>,
   document.getElementById('root')
 );
